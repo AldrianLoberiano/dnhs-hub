98f1b90156dfc3f39c77ce8814c3ffedb8d799a2
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-19 01:37:11 +0800

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -5,11 +5,25 @@
     </div>
     <!-- End Wrapper -->
     
+    <!-- Toast Alert Container -->
+    <div class="toast-container" id="toastContainer"></div>
+    
     <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
     <script src="<?php echo APP_URL; ?>/assets/js/app.js"></script>
+    
+    <?php
+    // Auto-show flash message as toast
+    $flash = getFlashMessage();
+    if ($flash):
+    ?>
+    <script>
+        showToast('<?php echo $flash["type"]; ?>', '<?php echo addslashes($flash["message"]); ?>');
+    </script>
+    <?php endif; ?>
+    
     <?php if (isset($extraScripts)): ?>
         <?php echo $extraScripts; ?>
     <?php endif; ?>
