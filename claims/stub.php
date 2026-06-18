c7f2cae74d6e3ef9cad410df7143bfe096367bb4
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-19 01:09:57 +0800

    Update stub.php

--- claims/stub.php
+++ claims/stub.php
@@ -5,7 +5,7 @@
  * Generate and display claim stub for document requests
  */
 
-require_once __DIR__ . '/../../config/config.php';
+require_once __DIR__ . '/../config/config.php';
 requireAuth();
 
 $db = getDBConnection();
