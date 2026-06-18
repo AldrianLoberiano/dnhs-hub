533ba59f8290dfbdff4191412346f40b91746e67
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-19 01:11:12 +0800

    Update restore.php

--- students/restore.php
+++ students/restore.php
@@ -5,7 +5,7 @@
  * Restore an archived student record
  */
 
-require_once __DIR__ . '/../../config/config.php';
+require_once __DIR__ . '/../config/config.php';
 requireAuth();
 
 $db = getDBConnection();
