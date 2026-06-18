12cea7d622af0bfd317244407f9bdf244deb6dd0
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-19 00:43:25 +0800

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,17 @@
+            </div>
+            <!-- End Main Content -->
+        </div>
+        <!-- End Page Content Wrapper -->
+    </div>
+    <!-- End Wrapper -->
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
+    <script src="assets/js/app.js"></script>
+    <?php if (isset($extraScripts)): ?>
+        <?php echo $extraScripts; ?>
+    <?php endif; ?>
+</body>
+</html>
