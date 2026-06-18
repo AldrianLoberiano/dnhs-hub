b6d47e2bd62d59f4cdaa89e812637c7b2e67d03f
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-19 01:11:05 +0800

    Update archived.php

--- students/archived.php
+++ students/archived.php
@@ -6,7 +6,7 @@
  */
 
 $pageTitle = 'Archived Students - DNHS Hub';
-require_once __DIR__ . '/../../includes/header.php';
+require_once __DIR__ . '/../includes/header.php';
 
 $db = getDBConnection();
 
@@ -120,4 +120,4 @@ $students = $stmt->fetchAll();
     </div>
 </div>
 
-<?php require_once __DIR__ . '/../../includes/footer.php'; ?>
+<?php require_once __DIR__ . '/../includes/footer.php'; ?>
