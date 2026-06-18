b8bda89d15ff40534c88ed84c18e9b0d501672d6
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-19 01:11:26 +0800

    Update toggle_status.php

--- users/toggle_status.php
+++ users/toggle_status.php
@@ -5,7 +5,7 @@
  * Activate/Deactivate a user
  */
 
-require_once __DIR__ . '/../../config/config.php';
+require_once __DIR__ . '/../config/config.php';
 requireAdmin();
 
 $db = getDBConnection();
