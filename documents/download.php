814c42103646e0fca0eeb5ec8722ca5fd53617ba
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-19 01:10:10 +0800

    Update download.php

--- documents/download.php
+++ documents/download.php
@@ -5,7 +5,7 @@
  * Download a student document
  */
 
-require_once __DIR__ . '/../../config/config.php';
+require_once __DIR__ . '/../config/config.php';
 requireAuth();
 
 $db = getDBConnection();
