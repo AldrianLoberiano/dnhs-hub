9f1806b17c5ef21d9515a280385c332758fe0d10
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-19 01:15:12 +0800

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -5,8 +5,8 @@
     </div>
     <!-- End Wrapper -->
     
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
     <script src="<?php echo APP_URL; ?>/assets/js/app.js"></script>
