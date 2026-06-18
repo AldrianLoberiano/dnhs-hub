6677c9fd2e2e3c57fbe140d458b1dd5dfc8bcc33
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-19 01:10:13 +0800

    Update index.php

--- documents/index.php
+++ documents/index.php
@@ -6,7 +6,7 @@
  */
 
 $pageTitle = 'Student Documents - DNHS Hub';
-require_once __DIR__ . '/../../includes/header.php';
+require_once __DIR__ . '/../includes/header.php';
 
 $db = getDBConnection();
 
@@ -170,4 +170,4 @@ $docTypes = $stmt->fetchAll();
     </div>
 </div>
 
-<?php require_once __DIR__ . '/../../includes/footer.php'; ?>
+<?php require_once __DIR__ . '/../includes/footer.php'; ?>
