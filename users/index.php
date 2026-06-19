008b67eabb4b8eed9340fb2e163eaee95c653c66
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-19 22:17:54 +0800

    Update index.php

--- users/index.php
+++ users/index.php
@@ -6,8 +6,9 @@
  */
 
 $pageTitle = 'User Management - DNHS Hub';
-require_once __DIR__ . '/../includes/header.php';
+require_once __DIR__ . '/../config/config.php';
 requireAdmin();
+require_once __DIR__ . '/../includes/header.php';
 
 $db = getDBConnection();
 
