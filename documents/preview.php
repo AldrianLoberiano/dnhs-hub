bf42334a9de383d3d7731e7eef4e363502ee7405
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-30 19:09:05 +0800

    Update preview.php

--- documents/preview.php
+++ documents/preview.php
@@ -26,11 +26,12 @@ if (!$doc) {
     redirect(APP_URL . '/documents/index.php');
 }
 
-$filePath = APP_ROOT . '/' . $doc['file_path'];
+$filePath = realpath(APP_ROOT . '/' . $doc['file_path']);
+$allowedDir = realpath(DOCUMENTS_PATH);
 
-// Fallback: check assets/uploads if file not found
-if (!file_exists($filePath)) {
-    $filePath = APP_ROOT . '/' . $doc['file_path'];
+if ($filePath === false || $allowedDir === false || strpos($filePath, $allowedDir) !== 0) {
+    setFlashMessage('error', 'Invalid file path.');
+    redirect(APP_URL . '/documents/index.php');
 }
 
 if (!file_exists($filePath)) {
@@ -50,6 +51,7 @@ $contentType = $contentTypes[$doc['file_type']] ?? 'application/octet-stream';
 
 // Send file for preview
 $safeFilename = preg_replace('/[^a-zA-Z0-9._-]/', '_', $doc['original_name']);
+$safeFilename = str_replace('"', '_', $safeFilename);
 header('Content-Type: ' . $contentType);
 header('Content-Length: ' . filesize($filePath));
 header('Content-Disposition: inline; filename="' . $safeFilename . '"');
