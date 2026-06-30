08223e5a13ffff414162ba447f8782f96c1a2740
Aldrian Loberiano <loberianorian@gmail.com>
2026-07-01 01:03:03 +0800

    Update profile.php

--- modules/auth/profile.php
+++ modules/auth/profile.php
@@ -62,7 +62,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 }
 
 $pageTitle = 'Profile - DNHS Hub';
-require_once __DIR__ . '/includes/header.php';
+require_once __DIR__ . '/../../includes/header.php';
 ?>
 
 <div class="page-header">
@@ -148,4 +148,4 @@ require_once __DIR__ . '/includes/header.php';
     </div>
 </div>
 
-<?php require_once __DIR__ . '/includes/footer.php'; ?>
+<?php require_once __DIR__ . '/../../includes/footer.php'; ?>
